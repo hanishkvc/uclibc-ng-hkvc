9cdf826bec2cd4ce73b6e9cb2cf21bde96fba31a
hanishkvc <hanishkvc@gmail.com>
2022-05-04 21:17:57 +0530

    Test dns lookup, reorder -I for better heirarchy and to find stuff

--- hkvc/test.c
+++ hkvc/test.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <netdb.h>
+
+void test_dns(char *sName) {
+	struct hostent *he = gethostbyname(sName);
+	printf("HN:%s\n",he->h_name);
+	printf("IP:%x\n",he->h_addr);
+}
 
 int main(int argc, char **argv) {
 	printf("hello world\n");
+	test_dns("www.google.com");
 	return 0;
 }
 
