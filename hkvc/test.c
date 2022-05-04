c00fd947f0c2ca37a34c7707cded7bc81d6de0ce
hanishkvc <hanishkvc@gmail.com>
2022-05-04 21:38:21 +0530

    TestDNS: Better gethostbyname handling, cmdline host name passing

--- hkvc/test.c
+++ hkvc/test.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 
 void test_dns(char *sName) {
 	struct hostent *he = gethostbyname(sName);
 	printf("HN:%s\n",he->h_name);
-	printf("IP:%x\n",he->h_addr);
+	int i=-1;
+	while(1) {
+		i += 1;
+		char *addr = he->h_addr_list[i];
+		if (addr == NULL) break;
+		printf("IP:%s\n",inet_ntoa(*(struct in_addr*)addr));
+	}
 }
 
 int main(int argc, char **argv) {
 	printf("hello world\n");
-	test_dns("www.google.com");
+	for(int i=1; i < argc; i++) {
+		test_dns(argv[i]);
+	}
 	return 0;
 }
 
