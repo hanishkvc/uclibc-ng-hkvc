c28d1d0215d35437851b012966c05fe338ff02ed
hanishkvc <hanishkvc@gmail.com>
2022-05-06 16:56:06 +0530

    TestDNS: handle hostname lookup failure
    Also cleanup the printed messages a bit.

--- hkvc/testdns.c
+++ hkvc/testdns.c
@@ -8,24 +8,28 @@
 
 void test_dns(char *sName) {
 	struct hostent *he = gethostbyname(sName);
-	printf("HN:%s\n",he->h_name);
+	if (he == NULL) {
+		printf("TestDNS:ERRR: GHbN:%s\n", sName);
+		return;
+	}
+	printf("TestDNS:INFO: HN:%s\n",he->h_name);
 	int i=-1;
 	while(1) {
 		i += 1;
 		char *addr = he->h_addr_list[i];
 		if (addr == NULL) break;
-		printf("IP:%s\n",inet_ntoa(*(struct in_addr*)addr));
+		printf("TestDNS:INFO: IP:%s\n",inet_ntoa(*(struct in_addr*)addr));
 	}
 }
 
 int main(int argc, char **argv) {
-	printf("TestDNS: hello world\n");
+	printf("TestDNS:INFO: hello world\n");
 	int sumGlobalRandoms = 0;
 	for(int i=1; i < argc; i++) {
 		test_dns(argv[i]);
 		sumGlobalRandoms += random();
 	}
-	printf("TestDNS: SumGlobalRandoms:%d\n", sumGlobalRandoms);
+	printf("TestDNS:INFO: SumGlobalRandoms:%d\n", sumGlobalRandoms);
 	return 0;
 }
 
